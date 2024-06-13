0e6ef84e6e85e6b0f5f9a2d72c9a2649b27cb3b5
hongjie <hongjie.lim@treeboxsolutions.com>
2024-06-13 18:59:09 +0800

    trouble update

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -5,10 +5,23 @@ end
 
 trouble.setup({
 	modes = {
-		diagnostics = {
+		mydiags = {
+			mode = "diagnostics", -- inherit from diagnostics mode
 			auto_open = true,
 			groups = {
 				{ "filename", format = "{file_icon} {basename:Title} {count}" },
+			},
+			filter = {
+				any = {
+					buf = 0, -- current buffer
+					{
+						severity = vim.diagnostic.severity.ERROR, -- errors only
+						-- limit to files in the current project
+						function(item)
+							return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+						end,
+					},
+				},
 			},
 		},
 	},
